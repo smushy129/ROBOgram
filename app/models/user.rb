29736dbfe9624387cdd511ca5a0111005e66689f
kingsleyliao <kingsleyyliao@gmail.com>
2017-06-20 10:08:13 -0400

    Setup frontend static view boilerplate

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,4 @@
-clasclass User < ActiveRecord::Base
+class User < ActiveRecord::Base
   validates :username, :password_digest, :session_token, presence: true
   validates :password, length: { minimum: 6, allow_nil: true }
 
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
     root "static_pages#root"
 
     namespace :api, defaults: {format: :json} do
-      resource :user, only: [:create]
-      resource :session, only: [:create, :destroy, :show]
+      resource :users, only: [:create, :update]
+      resource :session, only: [:create, :destroy]
     end
 end
