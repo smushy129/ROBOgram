a2de39f849fcdcf1a90ba954f7e55586dc4778b0
Kingsley Liao <kingsleyyliao@gmail.com>
2017-06-22 23:51:48 -0400

    start upload component

---
+++ frontend/components/upload_photo/upload_photo.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class uploadPhoto extends React.Component {
+  constructor(props){
+    super(props);
+  }
+
+  render() {
+    return (
+      <h1>dis is how you upload</h1>
+    );
+  }
+}
+
+export default uploadPhoto;
---
+++ frontend/components/upload_photo/upload_photo_container.js
@@ -0,0 +1,15 @@
+import { connect } from 'react-redux';
+import uploadPhoto from './upload_photo';
+
+const mapStateToProps = (state) => {
+
+};
+
+const mapDispatchToProps = (dispatch) => {
+
+};
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(uploadPhoto);
