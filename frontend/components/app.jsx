95f93bda46e7586a2a8baf27330ac983556e8dba
Kingsley Liao <kingsleyyliao@gmail.com>
2017-07-03 21:26:07 -0400

    Add production ready changes

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Route, Redirect, Switch, Link, HashRouter }
   from 'react-router-dom';
 import { AuthRoute, ProtectedRoute } from '../util/route_util';
-//components
+
 import NavbarContainer from './nav/navbar_container';
 import LoginFormContainer from './session_form/login_container';
 import SignupFormContainer from './session_form/signup_container';
--- frontend/robogram.jsx
+++ frontend/robogram.jsx
@@ -1,16 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-//Components
+
 import Root from './components/root';
 import configureStore from './store/store';
-
-//testing
-import { login, signup, logout } from './actions/session_actions';
-window.login = login;
-window.signup = signup;
-window.logout = logout;
-
-
 
 document.addEventListener('DOMContentLoaded', () => {
   const root = document.getElementById('root');
@@ -23,10 +15,6 @@ document.addEventListener('DOMContentLoaded', () => {
   } else {
     store = configureStore();
   }
-
-  window.getState = store.getState;
-  window.dispatch = store.dispatch;
-
 
   ReactDOM.render(<Root store={ store }/>, root);
 });
